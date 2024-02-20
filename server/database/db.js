59f3cc0fb790ad628de3f043a24d5daf921dad54
bhaveshjain144 <bhaveshjain1520.com>
2024-02-20 20:52:03 +0530

    Changes

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // const Connection = async (username, password) => {
 //     const URL = `mongodb://${username}:${password}@ecommerce-web-shard-00-00.wnaj9.mongodb.net:27017,ecommerce-web-shard-00-01.wnaj9.mongodb.net:27017,ecommerce-web-shard-00-02.wnaj9.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-sjmqa0-shard-0&authSource=admin&retryWrites=true&w=majority`
 const Connection = async () => {
-    const URL = `mongodb+srv://bj14decjain:WebsiteE-Commerce@cluster0.3gla8dh.mongodb.net/`
+    const URL = process.env.URI
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
         console.log('Database Connected Succesfully');
--- server/index.js
+++ server/index.js
@@ -37,5 +37,5 @@ paytmParams['ORDER_ID'] = uuid(),
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
 paytmParams['TXN_AMOUNT'] = '100',
 paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
-paytmParams['EMAIL'] = 'kunaltyagi@gmail.com'
+paytmParams['EMAIL'] = 'bhaveshjain1508@gmail.com'
 paytmParams['MOBILE_NO'] = '1234567852'
